0eba218f928fbe81e040e60730b4107269bac62a
Elena Kanash <hird250@gmail.com>
2023-11-14 20:31:07 +0300

    add retries to cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,10 @@ module.exports = defineConfig({
   watchForFileChanges: false,
   viewportHeight: 1080,
   viewportWidth: 1920,
+  retries:{
+    runMode: 1,
+    openMode: 1
+  },
   e2e: {
     baseUrl: 'https://demoqa.com/',
     setupNodeEvents(on, config) {      
