233289cba0405c220fa92f8ea4cee63e82d4a8af
Elena Kanash <hird250@gmail.com>
2024-02-10 22:42:48 +0300

    add Login to DemoQA

--- cypress/e2e/API.cy.js
+++ cypress/e2e/API.cy.js
@@ -1,44 +1,49 @@
 /// <reference types="cypress" />
 
 describe('API testing', () => {
-  let token, expires  
-  
+  let token, expires;
+  let userName = 'Elena';
+  let userPassword = '!Qwerty987+654';
+
   it('Authorized', () => {
     cy.request({
       method: 'POST',
       url: 'https://demoqa.com/Account/v1/Authorized',
       headers: {},
       body: {
-        "userName": "Elena",
-        "password": "!Qwerty987+654"
+        "userName": userName,
+        "password": userPassword,
       }
     }).then((response) => {
       expect(response.status).to.be.equal(200);
-      console.log(response)      
+      console.log(response)
     })
   });
 
-it('GenerateToken', () => {
-  cy.request({
-    method: 'POST',
-    url: 'https://demoqa.com/Account/v1/GenerateToken',
-    body: {
-      "userName": "Elena",
-      "password": "!Qwerty987+654"
-    }
-  }).then((response) => {
-    expect(response.status).to.be.equal(200);
-    console.log(response);
-    token = response.body.token;
-    expires = response.body.expires;
-    console.log(token)     
-    console.log(expires) 
-  })
-});
+  it('GenerateToken', () => {
+    cy.request({
+      method: 'POST',
+      url: 'https://demoqa.com/Account/v1/GenerateToken',
+      body: {
+        "userName": userName,
+        "password": userPassword,
+      }
+    }).then((response) => {
+      expect(response.status).to.be.equal(200);
+      console.log(response);
+      token = response.body.token;
+      expires = response.body.expires;
+      console.log({ token, expires });
+    })
+  });
 
-it('Login to DemoQA', () => {
-  
-});
+  it('Login to DemoQA', () => {
+    cy.setCookie('userName', userName);
+    cy.setCookie('token', token);
+    cy.setCookie('expires', expires);
+    cy.visit('https://demoqa.com/books');
+    cy.get('#userName-value').should('contain', userName)
+  });
 
 });
 
