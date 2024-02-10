593f5e6818f1a585031314af9df95eb850418114
Elena Kanash <hird250@gmail.com>
2024-02-10 22:03:24 +0300

    add GenerateToken

--- cypress/e2e/API.cy.js
+++ cypress/e2e/API.cy.js
@@ -1,8 +1,9 @@
 /// <reference types="cypress" />
 
 describe('API testing', () => {
+  let token, expire  
   
-  it('Login', () => {
+  it('Authorized', () => {
     cy.request({
       method: 'POST',
       url: 'https://demoqa.com/Account/v1/Authorized',
@@ -16,5 +17,25 @@ describe('API testing', () => {
       console.log(response)      
     })
   });
+
+it('GenerateToken', () => {
+  cy.request({
+    method: 'POST',
+    url: 'https://demoqa.com/Account/v1/GenerateToken',
+    body: {
+      "userName": "Elena",
+      "password": "!Qwerty987+654"
+    }
+  }).then((response) => {
+    expect(response.status).to.be.equal(200);
+    console.log(response);
+    token = response.body.token;
+    expire = response.body.expire;
+    console.log(token)     
+    console.log(expire) 
+  })
+});
+
+
 });
 
